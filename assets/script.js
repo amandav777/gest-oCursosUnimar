dd3f9a4cb6d1da5c7930c2993fc16f82e6955cdf
Amanda Vieira <108702135+amandav777@users.noreply.github.com>
2024-03-24 17:17:03 -0300

    feat: correções de javascript

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,27 @@
-
-
 const exampleModal = document.getElementById('exampleModal')
+const excludeModal = document.getElementById('excludeModal')
+const createModal  = document.getElementById('createModal')
+
 if (exampleModal) {
   exampleModal.addEventListener('show.bs.modal', event => {
-    // Button that triggered the modal
     const button = event.relatedTarget
-    // Extract info from data-bs-* attributes
     const recipient = button.getAttribute('data-bs-whatever')
-    // If necessary, you could initiate an Ajax request here
-    // and then do the updating in a callback.
-
-    // Update the modal's content.
     const modalTitle = exampleModal.querySelector('.modal-title')
     const modalBodyInput = exampleModal.querySelector('.modal-body input')
-
-    modalTitle.textContent = `New message to ${recipient}`
-    modalBodyInput.value = recipient
-
-    
+  })
+}
+if (excludeModal) {
+  exampleModal.addEventListener('show.bs.modal', event => {
+    const button = event.relatedTarget
+    const recipient = button.getAttribute('data-bs-whatever')
+    const modalTitle = exampleModal.querySelector('.modal-title')
   })
 }
 
+if (createModal) {
+  exampleModal.addEventListener('show.bs.modal', event => {
+    const button = event.relatedTarget
+    const recipient = button.getAttribute('data-bs-whatever')
+    const modalTitle = exampleModal.querySelector('.modal-title')
+  })
+}
